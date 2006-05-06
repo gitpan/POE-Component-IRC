ccde97680c730280ef6f43935c6e21200aeaa1cd
Chris Williams <chris@bingosnet.co.uk>
2006-05-06 16:13:30 +0000

    Import of BINGOS/POE-Component-IRC-4.87 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC
    gitpan-cpan-version:      4.87
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-4.87.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC.pm
+++ lib/POE/Component/IRC.pm
@@ -33,8 +33,8 @@ use vars qw($VERSION $REVISION $GOT_SSL $GOT_CLIENT_DNS);
 # Load the plugin stuff
 use POE::Component::IRC::Plugin qw( :ALL );
 
-$VERSION = '4.86';
-$REVISION = do {my@r=(q$Revision: 142 $=~/\d+/g);sprintf"%d"."%04d"x$#r,@r};
+$VERSION = '4.87';
+$REVISION = do {my@r=(q$Revision: 152 $=~/\d+/g);sprintf"%d"."%04d"x$#r,@r};
 
 # BINGOS: I have bundled up all the stuff that needs changing for inherited classes
 # 	  into _create. This gets called from 'spawn'.
@@ -143,6 +143,7 @@ sub _create {
 				      kick
 				      register
 				      register_session
+				      remove
 				      shutdown
 				      sl
 				      sl_login
@@ -1107,6 +1108,21 @@ sub kick {
 
   $nick .= " :$message" if defined $message;
   $kernel->yield( 'sl_high', "KICK $chan $nick" );
+  undef;
+}
+
+# Tell the IRC server to forcibly remove a user from a channel. Freenode extension
+sub remove {
+  my ($kernel, $chan, $nick) = @_[KERNEL, ARG0, ARG1];
+  my $message = join '', @_[ARG2 .. $#_];
+
+  unless (defined $chan and defined $nick) {
+    warn "The POE::Component::IRC event \"remove\" requires at least two arguments";
+    return;
+  }
+
+  $nick .= " :$message" if defined $message;
+  $kernel->yield( 'sl_high', "REMOVE $chan $nick" );
   undef;
 }
 
@@ -2314,6 +2330,13 @@ Tell the IRC server to forcibly evict a user from a particular
 channel. Takes at least 2 arguments: a channel name, the nick of the
 user to boot, and an optional witty message to show them as they sail
 out the door.
+
+=item remove ( Freenode only )
+
+Tell the IRC server to forcibly evict a user from a particular
+channel. Takes at least 2 arguments: a channel name, the nick of the
+user to boot, and an optional witty message to show them as they sail
+out the door. Similar to KICK but does an enforced PART instead.
 
 =item mode
 
