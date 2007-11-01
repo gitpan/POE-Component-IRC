32d21a9aef8dd3ef42784d3ca6054ab69b78361d
Chris Williams <chris@bingosnet.co.uk>
2007-11-01 14:32:47 +0000

    Import of BINGOS/POE-Component-IRC-5.36 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC
    gitpan-cpan-version:      5.36
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-5.36.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC.pm
+++ lib/POE/Component/IRC.pm
@@ -33,8 +33,8 @@ use vars qw($VERSION $REVISION $GOT_SSL $GOT_CLIENT_DNS);
 # Load the plugin stuff
 use POE::Component::IRC::Plugin qw( :ALL );
 
-$VERSION = '5.34';
-$REVISION = do {my@r=(q$Revision: 340 $=~/\d+/g);sprintf"%d"."%04d"x$#r,@r};
+$VERSION = '5.36';
+$REVISION = do {my@r=(q$Revision: 342 $=~/\d+/g);sprintf"%d"."%04d"x$#r,@r};
 
 # BINGOS: I have bundled up all the stuff that needs changing for inherited classes
 # 	  into _create. This gets called from 'spawn'.
